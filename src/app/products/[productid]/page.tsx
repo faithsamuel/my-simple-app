2e99e549b602471e6dcde9c55b74f47ec96b6618
Faith Samuel <faithsamuel365@gmail.com>
2025-02-19 00:00:33 +0100

    Syntax update

--- src/app/products/[productid]/page.tsx
+++ src/app/products/[productid]/page.tsx
@@ -1,9 +1,8 @@
 
-export default async function ProductDetails(
-    {
+export default async function ProductDetails({
     params,
 }: {
-    params: Promise<{ productId: string}>
+    params: Promise<{ productId: string}>;
 }
 ) {
   const productId = (await params).productId;
